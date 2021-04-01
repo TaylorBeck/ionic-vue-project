9920d72146eea0f91a73e5261b6284af22047ae5
Taylor Beck <beck.taylorg@gmail.com>
2021-04-01 14:19:33 -0700

    Clean up testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,6 @@ module.exports = {
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
       env: {
         jest: true
       }
