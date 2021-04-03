114693e484e78013e4b2753a9556bb8c90f7a71c
Taylor Beck <beck.taylorg@gmail.com>
2021-04-02 17:41:38 -0700

    Add Vue devServer public config

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,9 @@ const { version, description, versionCodename } = require("./package.json");
 process.env.VUE_APP_VERSION = version;
 process.env.VUE_APP_DESCRIPTION = description;
 process.env.VUE_APP_CODENAME = versionCodename;
+
+module.exports = {
+  devServer: {
+    public: 'classroom-manager-vue-ionic.herokuapp.com:8100'
+  }
+};
