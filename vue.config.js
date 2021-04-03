49bae8db262bde706d7bc609816ea8eeb5eeeb53
Taylor Beck <beck.taylorg@gmail.com>
2021-04-02 23:16:09 -0700

    Add publicPort alternative port

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ process.env.VUE_APP_VERSION = version;
 process.env.VUE_APP_DESCRIPTION = description;
 process.env.VUE_APP_CODENAME = versionCodename;
 
-const publicPort = `classroom-manager-vue-ionic.herokuapp.com:${process.env.PORT}`;
+const publicPort = `classroom-manager-vue-ionic.herokuapp.com:${process.env.PORT || 8100}`;
 module.exports = {
   devServer: {
     public: publicPort
